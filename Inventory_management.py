7fa36fbe7ad273807e9427cc96756c86e5b4b2e2
avishekpaul1310 <avishekpaul1310@gmail.com>
2024-09-03 09:39:50 +0530

    first commit

---
+++ Inventory_management.py
@@ -0,0 +1,12 @@
+"""new_shipments = [("apples", 5), ("bananas", 10), ("pears", 7) ]
+
+for item in new_shipments:
+    print(item)
+    print(item[0])
+    print(type(item))
+    print(type(item[0]))
+    print(type(item[1]))"""
+
+#x = (type(''),type(int))
+#print(type(x))
+x = input('please enter the name of new item and quantity:')
