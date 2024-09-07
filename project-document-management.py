7c538edb5e2294b47cf19d9a08fcc5406d2464f6
avishekpaul1310 <avishekpaul1310@gmail.com>
2024-09-07 10:07:14 +0530

    verify

--- project-document-management.py
+++ project-document-management.py
@@ -1,17 +1,3 @@
-"""new_shipments = [("apples", 5), ("bananas", 10), ("pears", 7) ]
-
-for item in new_shipments:
-    print(item)
-    print(item[0])
-    print(type(item))
-    print(type(item[0]))
-    print(type(item[1]))"""
-
-#x = (type(''),type(int))
-#print(type(x))
-#x = input('please enter the name of new item and quantity:')
-
-#def update_documents(documents,new_docs):
 
 documents = { 
     "Project Plan": "Draft",
@@ -27,6 +13,9 @@ for x in New_docs:
     if x[0] not in documents.keys():
         documents.update({x[0]:x[1]}) 
 print(documents)
+
+
+
 
 
 
